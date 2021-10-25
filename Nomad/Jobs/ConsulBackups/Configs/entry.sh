eeecdeafeeb619ff988d0f35765deae9ca96fa63
Kristian Jones <me@kristianjones.dev>
2021-10-25 09:50:09 +0000

    fix: Minor changes

--- Nomad/Jobs/ConsulBackups/Configs/entry.sh
+++ Nomad/Jobs/ConsulBackups/Configs/entry.sh
@@ -1,3 +1,7 @@
 #!/bin/sh
+DATE=""
+
+echo "consul-backinator backup -file s3://${S3.Bucket}/backup-$(date +%m%d%Y.%s).bak?endpoint=${S3.Connection.Hostname}:${S3.Connection.Port}&secure=false"
+
 
 consul-backinator backup -file s3://${S3.Bucket}/backup-$(date +%m%d%Y.%s).bak?endpoint=${S3.Connection.Hostname}:${S3.Connection.Port}&secure=false
