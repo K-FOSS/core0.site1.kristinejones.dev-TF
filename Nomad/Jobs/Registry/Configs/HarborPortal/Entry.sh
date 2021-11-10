14580e0a53650a9f5aaa7bdbca33ca3cf8f8f9d9
Kristian Jones <me@kristianjones.dev>
2021-11-10 06:42:51 +0000

    fix

--- Nomad/Jobs/Registry/Configs/HarborPortal/Entry.sh
+++ Nomad/Jobs/Registry/Configs/HarborPortal/Entry.sh
@@ -2,6 +2,10 @@
 
 echo "HelloWorld"
 
+mkdir -p /var/log/nginx
+
+chown nginx:nginx /var/log/nginx
+
 cp /local/Harbor/NGINX.conf  /etc/nginx/nginx.conf
 
-exec su -p -c "nginx -g daemon off;" nginx
+exec su -p -c "/usr/sbin/nginx -g 'daemon off;'" nginx
