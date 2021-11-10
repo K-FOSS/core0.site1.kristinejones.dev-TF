f48fe4c8722eaf1ae32ee3742d5c2468e6fa070b
Kristian Jones <me@kristianjones.dev>
2021-11-10 06:57:54 +0000

    fix

--- Nomad/Jobs/Registry/Configs/HarborPortal/Entry.sh
+++ Nomad/Jobs/Registry/Configs/HarborPortal/Entry.sh
@@ -5,6 +5,8 @@ echo "HelloWorld"
 mkdir -p /var/log/nginx
 
 chown nginx:nginx /var/log/nginx
+chmod 777 /var/log/nginx
+chmod 777 -R /var/log/nginx
 
 cp /local/Harbor/NGINX.conf  /etc/nginx/nginx.conf
 
