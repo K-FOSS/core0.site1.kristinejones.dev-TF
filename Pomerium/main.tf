f1585f6f9ac8c528cbcdd3649a971ab31d8faeb4
Kristian Jones <me@kristianjones.dev>
2021-11-12 07:37:37 +0000

    fix

--- Pomerium/main.tf
+++ Pomerium/main.tf
@@ -35,33 +35,6 @@ resource "random_string" "SharedSecret" {
 }
 
 locals {
-  Services = tomap({
-    Authenticate = {
-      Name = "authenticate"
-      Count = 3
-
-      TLS = var.TLS.Authenticate
-    },
-    Proxy = {
-      Name = "proxy"
-      Count = 3
-
-      TLS = var.TLS.Proxy
-    },
-    Authorize = {
-      Name = "authorize"
-      Count = 3
-
-      TLS = var.TLS.Authorize
-    },
-    DataBroker = {
-      Name = "databroker"
-      Count = 3
-
-      TLS = var.TLS.DataBroker
-    }
-  })
-
   Secrets = {
     CookieSecret = base64encode(random_string.CookieSecret.result)
     SharedSecret = base64encode(random_string.SharedSecret.result)
