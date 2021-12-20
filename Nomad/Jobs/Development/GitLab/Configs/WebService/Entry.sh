c43050d45e6ae6c69610a633d365647f97e802a8
Kristian Jones <me@kristianjones.dev>
2021-12-20 08:01:11 +0000

    fix

--- Nomad/Jobs/Development/GitLab/Configs/WebService/Entry.sh
+++ Nomad/Jobs/Development/GitLab/Configs/WebService/Entry.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
-cat /local/TLS/RegistryCA.pem
+cat /local/TLS/RegistryCA.pem >> /etc/ssl/certs/ca-certificates.crt
+cat secrets/TLS/CA.pem >> /etc/ssl/certs/ca-certificates.crt
 
 exec /scripts/entrypoint.sh /bin/sh -c /scripts/process-wrapper
