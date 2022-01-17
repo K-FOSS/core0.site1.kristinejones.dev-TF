91411ec2faa14334341050c521d81edabd96b052
Kristian Jones <me@kristianjones.dev>
2022-01-17 05:46:37 +0000

    fix

--- Nomad/Jobs/Network/Monitoring/OpenNMS/main.tf
+++ Nomad/Jobs/Network/Monitoring/OpenNMS/main.tf
@@ -83,6 +83,11 @@ locals {
 
           File = file("${path.module}/Configs/OpenNMS/Minion/SinglePort.cfg")
         },
+        Netflow9 = {
+          Path = "org.opennms.features.telemetry.listeners-udp-4729.cfg"
+
+          File = file("${path.module}/Configs/OpenNMS/Minion/Netflow9.cfg")
+        },
         KafkaIPC = {
           Path = "org.opennms.core.ipc.sink.kafka.cfg"
 
